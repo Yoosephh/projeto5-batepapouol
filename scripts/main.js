5109565ce7c2d159eb3b8c113e245c6809dde1ea
Yoosephh <jose.es.oleg@gmail.com>
2023-04-13 09:46:03 -0300

    commit atualizado JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,7 @@
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0'); 
-    const minutes = now.getMinutes().toString().padStart(2, '0'); 
-    const seconds = now.getSeconds().toString().padStart(2, '0'); 
-    const time_str = `${hours}:${minutes}:${seconds}`;
-    console.log(`agora sao  ${time_str}`)
+const now = new Date();
+const hours = now.getHours().toString().padStart(2, '0'); 
+const minutes = now.getMinutes().toString().padStart(2, '0'); 
+const seconds = now.getSeconds().toString().padStart(2, '0'); 
+const time_str = `${hours}:${minutes}:${seconds}`;
 
 const seuNome = prompt('Digite um nome ou apelido:')
