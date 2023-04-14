e67c50708ee610e82ae6f02895e68cb51c71d06a
Yoosephh <jose.es.oleg@gmail.com>
2023-04-13 22:59:07 -0300

    Versao entrando no servidor

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,79 @@
-const now = new Date();
-const hours = now.getHours().toString().padStart(2, '0'); 
-const minutes = now.getMinutes().toString().padStart(2, '0'); 
-const seconds = now.getSeconds().toString().padStart(2, '0'); 
-const time_str = `${hours}:${minutes}:${seconds}`;
+let enviaMsg = document.getElementById('enviarMsg');
 
-const seuNome = prompt('Digite um nome ou apelido:')
+let textoMsg 
+
+let campoMsg = document.querySelector('input')
+
+let estruturaMsgs = document.getElementById('msgs');
+
+let seuNome = prompt('Digite um nome ou apelido:')
+axios.defaults.headers.common['Authorization'] = '';
+usuarioEstaOnline()
+
+
+function usuarioEstaOnline() {
+    let envioSeuNome
+    console.log("testando")
+
+        let quemEntrou = {name : seuNome}
+        envioSeuNome = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', quemEntrou)
+
+        envioSeuNome.then(entraNaSala);
+
+        envioSeuNome.catch(() => {
+            alert('O nome utilizado esta indisponivel!')
+            window.location.reload()
+        })
+}
+
+function escolhaOutroNome(){
+    seuNome=prompt('O nome ou apelido escolhido esta indisponivel! Tente novamente com outro:')
+}
+let contentTypeStatus = {
+    from: seuNome,
+    to: "Todos",
+    text: "entra na sala...",
+    type: "status",
+    time: " "
+}
+let contentTypeMessage = {
+    from: seuNome,
+    to: "Todos",
+    text: " ",
+    type: "message"
+}
+let contentTypePMessage = {
+    from: seuNome,
+    to: "Todos",
+    text: " ",
+    type: "private-message",
+    time: " "
+}
+enviaMsg.addEventListener('click', sendMsg);
+campoMsg.addEventListener('keydown', function(event) {
+    if (event.key === "Enter") {sendMsg()}
+})
+function sendMsg() {
+    textoMsg = document.getElementById('digitarMsg').value;
+    contentTypeMessage.text = textoMsg;
+
+    let msgEnviada = axios.post("https://mock-api.driven.com.br/api/vm/uol/messages", contentTypeMessage)
+
+    msgEnviada.then(mostrarMsg);
+    msgEnviada.catch(showError);
+}
+
+function mostrarMsg(){
+    console.log("Deu certo!")
+}
+
+function showError(error) {
+    console.log(error.response.data)
+}
+function entraNaSala() {
+    setInterval(() => {axios.post('https://mock-api.driven.com.br/api/vm/uol/status', {
+        name: seuNome
+    })
+    .then((res) => console.log(res))}
+    ,2000)
+}
